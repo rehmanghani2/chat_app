4e04a0e7ae536575f01ddb597563756d60b8fdc8
rehmanghani2 <rehmanghani2323@gmail.com>
2025-08-04 14:40:54 +0500

    updates inserver

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ await connectDB();
 
 if(process.env.NODE_ENV !== "production") {
     const PORT = process.env.PORT || 5000;
-    server.listen(PORT, ()=> console.log("Server is running on PORT: "+PORT));
+    server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT));
 }
 // Export server for Vercel
 export default server;
